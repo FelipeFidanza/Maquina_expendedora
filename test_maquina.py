640fad9a2fd8489bd3b3b24bdc0750663e948f3d
Felipe Fidanza <ffidanzar@gmail.com>
2025-06-03 14:41:55 -0300

    modifico test de vuelto (falso)

--- test_maquina.py
+++ test_maquina.py
@@ -14,22 +14,22 @@ class TestMaquinaExpendedora(unittest.TestCase):
         self.maquina.total_ingresado = 50
 
     def test_ingresa_mas_billetes(self):
-        self.assertFalse(self.maquina.comprueba_billetes([1, 3, 2, 0]))
-        self.assertFalse(self.maquina.comprueba_billetes([0, 1, 4, 2]))
-        self.assertFalse(self.maquina.comprueba_billetes([4, 1, 0, 3]))
-        self.assertFalse(self.maquina.comprueba_billetes([5, 3, 0, 0]))
-        self.assertFalse(self.maquina.comprueba_billetes([8, 0, 1, 0]))
+        self.assertFalse(self.maquina.comprueba_billetes([1, 3, 2, 0])) # Devuelve falso por exceso de billetes 
+        self.assertFalse(self.maquina.comprueba_billetes([0, 1, 4, 2])) # Devuelve falso por exceso de billetes
+        self.assertFalse(self.maquina.comprueba_billetes([4, 1, 0, 3])) # Devuelve falso por exceso de billetes
+        self.assertFalse(self.maquina.comprueba_billetes([5, 3, 0, 0])) # Devuelve falso por exceso de billetes
+        self.assertFalse(self.maquina.comprueba_billetes([8, 0, 1, 0])) # Devuelve falso por exceso de billetes
 
     def test_ingresa_menos_billetes(self):
-        self.assertTrue(self.maquina.comprueba_billetes([0, 0, 3, 0]))
-        self.assertTrue(self.maquina.comprueba_billetes([1, 2, 0, 1]))
-        self.assertTrue(self.maquina.comprueba_billetes([0, 0, 1, 0]))
-        self.assertTrue(self.maquina.comprueba_billetes([1, 1, 1, 1]))
-        self.assertTrue(self.maquina.comprueba_billetes([1, 1, 1, 2]))
+        self.assertTrue(self.maquina.comprueba_billetes([0, 0, 3, 0])) # Devuelve verdadero a la cantidad de billetes
+        self.assertTrue(self.maquina.comprueba_billetes([1, 2, 0, 1])) # Devuelve verdadero a la cantidad de billetes
+        self.assertTrue(self.maquina.comprueba_billetes([0, 0, 1, 0])) # Devuelve verdadero a la cantidad de billetes
+        self.assertTrue(self.maquina.comprueba_billetes([1, 1, 1, 1])) # Devuelve verdadero a la cantidad de billetes
+        self.assertTrue(self.maquina.comprueba_billetes([1, 1, 1, 2])) # Devuelve verdadero a la cantidad de billetes
 
     def test_devuelve_vuelto(self):
-        self.assertEqual(self.maquina._MaquinaExpendedora__devuelve_vuelto(18), [1,1,1,1])
-        self.assertEqual(self.maquina._MaquinaExpendedora__devuelve_vuelto(14), [0,2,0,1])
+        self.assertEqual(self.maquina._MaquinaExpendedora__devuelve_vuelto(18), [1,1,1,0]) # Vuelto de $18 en 1 billete de cada tipo
+        self.assertEqual(self.maquina._MaquinaExpendedora__devuelve_vuelto(14), [0,2,0,1]) # Vuelto de $14 en dos de $2 y uno de $10
 
     # def test_recarga_existencias(self):
     #     self.assertEqual(self.maquina.recarga_existencias(20, 3), 'Artículos recargados. cantidad sobrante: 0.')
