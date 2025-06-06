ebf2d9ee9ae230ba60f539f70597109cd9c0d21f
Felipe Fidanza <ffidanzar@gmail.com>
2025-06-05 21:16:21 -0300

    agrego test de recarga de existencia

--- test_maquina.py
+++ test_maquina.py
@@ -31,9 +31,9 @@ class TestMaquinaExpendedora(unittest.TestCase):
         self.assertEqual(self.maquina._MaquinaExpendedora__devuelve_vuelto(18), [1,1,1,1]) # Vuelto de $18 en 1 billete de cada tipo
         self.assertEqual(self.maquina._MaquinaExpendedora__devuelve_vuelto(14), [0,2,0,1]) # Vuelto de $14 en dos de $2 y uno de $10
 
-    # def test_recarga_existencias(self):
-    #     self.assertEqual(self.maquina.recarga_existencias(20, 3), 'Artículos recargados. cantidad sobrante: 0.')
-    #     self.assertEqual(self.maquina.recarga_existencias(15, 1), 'Artículos recargados. cantidad sobrante: 4.')
+    def test_recarga_existencias(self):
+        self.assertEqual(self.maquina.recarga_existencias(20, 3), 'Artículos recargados. cantidad sobrante: 0.')
+        self.assertEqual(self.maquina.recarga_existencias(15, 1), 'Artículos recargados. cantidad sobrante: 4.')
 
 if __name__ == '__main__':
     unittest.main()
